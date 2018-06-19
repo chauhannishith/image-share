a556bdb5ee95ca8159fabd9ec0d376c1be34f7d6
chauhannishith <chauhannis123ith@gmail.com>
2018-06-19 12:34:15 +0530

    display images in their specified group

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -1,18 +1,39 @@
 import React, { Component} from 'react'
 import DragnDrop from './DragnDrop'
+import { BACKEND } from '../utils/config'
 
 class Images extends Component {
 
 	constructor(props) {
 		super(props)
-
+		this.state ={
+			groupimages: []
+		}
 	}
 
+	componentDidMount(){
+		let localImages = []
+		let x
+		for(let i = 0; i < this.props.images.length ; i++) {
+			x = this.props.images[i].metadata.subgroup
+			if(x === this.props.groupTitle){
+				localImages.push(this.props.images[i])
+				// console.log(this.props.images.length)	
+			}
+		}
+		setTimeout(() => this.setState({groupimages: [...localImages]}), 2000)
+		
+	}
+//				<h4>{this.props.projectId} {this.props.groupTitle}</h4>
 	render() {
+		const eachGroupImage = this.state.groupimages.map((image, i) => {
+			return <img key={i} src={`${BACKEND}` + '/api/users/images/' + `${image.filename}`} alt="noimage.jpg" className="thumb" />
+		})
+
 		return (
 			<div>
-				<h1>Display image</h1>
-				<h4>{this.props.projectId} {this.props.groupTitle}</h4>
+				<h6>Display image</h6>
+				{eachGroupImage}
 				<DragnDrop projectid={this.props.projectId} subgroup={this.props.groupTitle}/>
 			</div>
 			)
--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -98,26 +98,24 @@ class Project extends Component{
 		
 	}
 
-	// toggleState() {
-	// 	let temp = !this.state.displayDrop
-	// 	if(temp === true)
-	// 		document.getElementById('display').innerText = 'Hide'
-	// 	else
-	// 		document.getElementById('display').innerText = 'View'
-	// 	this.setState({displayDrop: temp})
-	// }
-	// 			<button id="display" onClick={this.toggleState.bind(this)}>View</button>	
-	// 			{this.state.displayDrop && <DragnDrop projectid={this.props.location.state.projectId} />}
 	render() {
 		const eachImage = this.state.images.map((image, i) => {
-			return <img key={i} src={`${BACKEND}` + '/api/users/images/' + `${image.filename}`} alt="noimage.jpg" className="thumb" />
+			if(!image.metadata.subgroup){
+				return <img key={i} src={`${BACKEND}` + '/api/users/images/' + `${image.filename}`} alt="noimage.jpg" className="thumb" />
+			}
+			else{
+				return null
+			}
 		})
 
 		const eachGroup = this.state.subgroups.map((group, i) => {
 			return (
 				<li key={i}>
-					{group.groupTitle}
-					<Images projectId={this.props.location.state.projectId} groupTitle={group.groupTitle} />
+					<h3>
+						{group.groupTitle}
+					</h3>
+					<Images projectId={this.props.location.state.projectId} groupTitle={group.groupTitle}
+						images={this.state.images} />
 				</li>
 			)
 		})
