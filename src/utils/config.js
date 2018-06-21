67b38512b575fc30d59e59a72e97347d6935ccfc
chauhannishith <chauhannis123ith@gmail.com>
2018-06-21 15:07:51 +0530

    added backend link

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const hostname = window && window.location && window.location.hostname;
 
 let backendUrl;
 
-if(hostname === 'heroku front') {
+if(hostname === 'https://imagesharre.herokuapp.com/') {
   backendUrl = 'https://groupphotos.herokuapp.com';
 } else {
   backendUrl = process.env.REACT_APP_LOCAL_BACKEND || 'http://localhost:3001';
